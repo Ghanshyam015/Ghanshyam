e629c8683fdbca8fd872a1725dfa0841ac01574e
ghanshyam baria <121920915+Ghanshyam015@users.noreply.github.com>
2023-03-23 00:40:08 +0530

    Create Ghadiya.cpp

---
+++ Ghadiya.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,N;
+    cout<<"Number Of Ghadiya : ";
+    cin>>A;
+    for(int i=1 ; i<=10 ; i++)
+    {
+        cout<<" "<<A<<": "<<i<<": "<<A*i<<"\n" ; 
+    }
+}
